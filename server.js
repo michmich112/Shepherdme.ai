2842db95912619392766a088c75ae7ef60b6372c
AllaPranathi <88225874+AllaPranathi@users.noreply.github.com>
2021-11-14 21:05:08 -0800

    post call - RB changes

--- server.js
+++ server.js
@@ -4,8 +4,11 @@ const path = require('path');
 
 const app = express();
 
+var proxy = require('express-http-proxy');
+
 // Serve only the static files fr0m the dist directory
 app.use(express.static(__dirname + '/dist/AbuseFlagger'));
+app.use('/api', proxy('http://169.51.206.176:32451/model/predict'));
 
 app.get('/*', function(req,res) {
     
