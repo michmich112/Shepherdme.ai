be45f42215590bab8feb6ff97afd17fe67acdf60
AllaPranathi <88225874+AllaPranathi@users.noreply.github.com>
2021-11-14 22:45:30 -0800

    Reverting back o old code to bypass 503 proxy issue

--- server.js
+++ server.js
@@ -4,37 +4,12 @@ const path = require('path');
 
 const app = express();
 
-//var proxy = require('express-http-proxy');
-
-const url = require('url');
-//const proxy = require('express-http-proxy');
-const proxy = require('http-proxy-middleware')
-
-
-// var apiProxy = proxy('/api', {target: 'http://169.51.206.176:32451/model/predict'});
-
-   var apiProxy = proxy('/api', {target: 'http://169.51.206.176:32451/model/predict'});
-
-// Serve only the static files fr0m the dist directory
+// Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/AbuseFlagger'));
-// app.use('/api', proxy('http://169.51.206.176:32451/model/predict',  {
-//     target: 'http://169.51.206.176:32451/model/predict'
-// }) );
-
-
-
-// New hostname+path as specified by question:
-// const apiProxy = proxy('http://169.51.206.176:32451/model/predict', {
-//     proxyReqPathResolver: req => url.parse(req.originalUrl).path
-// });
-
-//app.use('/api', apiProxy);
 
 app.get('/*', function(req,res) {
- 
-
-res.sendFile(path.join(__dirname+'/dist/AbuseFlagger/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/AbuseFlagger/index.html'));
 });
-app.use(apiProxy)
+
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
