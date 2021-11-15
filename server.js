4462fb8bbadbf4109bd49328a3bf1cea172820b0
AllaPranathi <88225874+AllaPranathi@users.noreply.github.com>
2021-11-14 21:57:32 -0800

    Update server.js

--- server.js
+++ server.js
@@ -4,22 +4,33 @@ const path = require('path');
 
 const app = express();
 
-var proxy = require('express-http-proxy');
+//var proxy = require('express-http-proxy');
 
-var apiProxy = proxy('/api', {target: 'http://169.51.206.176:32451/model/predict'});
+const url = require('url');
+const proxy = require('express-http-proxy');
+
+// var apiProxy = proxy('/api', {target: 'http://169.51.206.176:32451/model/predict'});
 
 
 // Serve only the static files fr0m the dist directory
 app.use(express.static(__dirname + '/dist/AbuseFlagger'));
 // app.use('/api', proxy('http://169.51.206.176:32451/model/predict',  {
-//     proxyReqPathResolver: req => url.parse(req.baseUrl).path
+//     target: 'http://169.51.206.176:32451/model/predict'
 // }) );
+
+
+
+// New hostname+path as specified by question:
+const apiProxy = proxy('http://169.51.206.176:32451/model/predict', {
+    proxyReqPathResolver: req => url.parse(req.originalUrl).path
+});
+app.use('/api', apiProxy);
 
 app.get('/*', function(req,res) {
  
 
 res.sendFile(path.join(__dirname+'/dist/AbuseFlagger/index.html'));
 });
-app.use(apiProxy)
+// app.use(apiProxy)
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
